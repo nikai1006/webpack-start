2839a05efb7aa63d4236841a22a93612f8acf757
nikai <keni1006@163.com>
2018-11-07 23:11:23 +0800

    mutil

--- README.md
+++ README.md
@@ -8,4 +8,4 @@
 #### 参考文献
 
 - [https://www.imooc.com/qadetail/210600](https://www.imooc.com/qadetail/210600)
-- 
+- [https://blog.csdn.net/a4_9527/article/details/81045791](https://blog.csdn.net/a4_9527/article/details/81045791)
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 var path = require("path")
 module.exports = {
-    entry: './src/script/main.js', //打包的入口文件
+    entry: ['./src/script/main.js', './src/script/b.js'], //打包的入口文件
     output: {//打包输出
-        path: path.resolve(__dirname,'./dist/js'),
+        path: path.resolve(__dirname, './dist/js'),
         filename: 'bundle.js'
-    }
+    },
+    mode: 'development'
 }
