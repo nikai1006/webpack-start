e6860ba1542b9e9c202ca93869dbb1abdb636d12
nikai <keni1006@163.com>
2018-11-07 23:00:28 +0800

    add doc

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 var path = require("path")
 module.exports = {
     entry: './src/script/main.js', //打包的入口文件
-    output: {
+    output: {//打包输出
         path: path.resolve(__dirname,'./dist/js'),
         filename: 'bundle.js'
     }
